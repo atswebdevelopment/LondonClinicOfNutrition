b55df74c3b32f3197b98c0c34ed725ea9de564ad
atswebdvelopment <adam@atswebdevelopment.com>
2018-03-14 16:39:30 +0000

    Uat fixes

--- LondonClinicOfNutrition/Scripts/main.js
+++ LondonClinicOfNutrition/Scripts/main.js
@@ -34,7 +34,7 @@ var blogs = {
         for (var i = 0; i < data.length; i++) {
             html += '<div class="boxes__box"><div class="boxes__content">' +
                 '<a class="boxes__link" href="' + data[i].url + '"></a>' +
-                '<span class="boxes__image bg-load" data-src="' + data[i].image + '"></span>' +
+                '<span class="boxes__image bg-load" data-src="' + data[i].image + '?width=767"></span>' +
                 '<div class="boxes__icon icon"><span class="svg-load" data-src="' + data[i].icon + '"></span></div>' +
                 '<span class="boxes__timestamp timestamp">' + data[i].date + '</span>' +
                 '<span class="boxes__title">' + data[i].name + '</span>' +
@@ -207,7 +207,7 @@ var recipes = {
         for (var i = 0; i < data.length; i++) {
             html += '<div class="boxes__box"><div class="boxes__content">' +
                 '<a class="boxes__link" href="' + data[i].url + '"></a>' +
-                '<span class="boxes__image bg-load" data-src="' + data[i].image + '"></span>' +
+                '<span class="boxes__image bg-load" data-src="' + data[i].image + '?width=767"></span>' +
                 '<div class="boxes__icon icon"><span class="svg-load" data-src="' + data[i].icon + '"></span></div>' +
                 '<span class="boxes__title">' + data[i].name + '</span>' +
                 '<span class="button button--secondary"><a>Read more</a></span>' +
--- LondonClinicOfNutrition/Views/Partials/Recipes/recipes.js
+++ LondonClinicOfNutrition/Views/Partials/Recipes/recipes.js
@@ -49,7 +49,7 @@ var recipes = {
         for (var i = 0; i < data.length; i++) {
             html += '<div class="boxes__box"><div class="boxes__content">' +
                 '<a class="boxes__link" href="' + data[i].url + '"></a>' +
-                '<span class="boxes__image bg-load" data-src="' + data[i].image + '"></span>' +
+                '<span class="boxes__image bg-load" data-src="' + data[i].image + '?width=767"></span>' +
                 '<div class="boxes__icon icon"><span class="svg-load" data-src="' + data[i].icon + '"></span></div>' +
                 '<span class="boxes__title">' + data[i].name + '</span>' +
                 '<span class="button button--secondary"><a>Read more</a></span>' +
