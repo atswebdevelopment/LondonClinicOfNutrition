88de395d9f85d422632ec3cac3ee5e478ae57a7f
Adam Southorn <adam@atswebdevelopment.com>
2018-05-03 17:43:45 +0100

    Bug fixes

--- LondonClinicOfNutrition/Scripts/main.js
+++ LondonClinicOfNutrition/Scripts/main.js
@@ -96,7 +96,16 @@ var contentScroller = {
 var controls = {
     init: function () {
         //Controls
-        
+        if ($(window).width() < 767) {
+            var buttons = $('.controls__center .buttons'),
+                width = 0;
+
+            buttons.find('li').each(function () {
+                width += ($(this).width() + 20);
+            });
+
+            buttons.css('width', width);
+        }
     }
 };
 /*
--- LondonClinicOfNutrition/Views/Partials/Controls/controls.js
+++ LondonClinicOfNutrition/Views/Partials/Controls/controls.js
@@ -7,6 +7,15 @@
 var controls = {
     init: function () {
         //Controls
-        
+        if ($(window).width() < 767) {
+            var buttons = $('.controls__center .buttons'),
+                width = 0;
+
+            buttons.find('li').each(function () {
+                width += ($(this).width() + 20);
+            });
+
+            buttons.css('width', width);
+        }
     }
 };
